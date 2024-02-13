f81efc3ffa0a96580335c46526722008572ff858
Ilya S <ISmeneger@yandex.ru>
2024-02-13 20:31:43 +0300

    13.02

---
+++ lessons/lesson7_step2.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.ID, 'treasure')
+    x_element_checked = x_element.get_attribute("valuex")
+
+    x = x_element_checked
+    y = calc(x)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element(By.CSS_SELECTOR, '#answer')
+    input1.send_keys(y)
+
+    # Заполняем кнопки checkbox и radiobutton и отправляем заполненную форму
+    checkbox = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    checkbox.click()
+
+    radiobutton = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    radiobutton.click()
+
+    submit_button = browser.find_element(By.CSS_SELECTOR, "button")
+    submit_button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
