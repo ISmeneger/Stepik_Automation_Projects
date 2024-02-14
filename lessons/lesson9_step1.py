f1a1b072df121a6e26c8f900cc6784a69057e9cc
Ilya S <ISmeneger@yandex.ru>
2024-02-14 18:57:36 +0300

    14.02

---
+++ lessons/lesson9_step1.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = 'http://suninjuly.github.io/alert_accept.html'
+
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    browser.find_element(By.CSS_SELECTOR, "div > button").click()
+
+    # Приимаем confirm
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    time.sleep(3)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, '#input_value')
+    x = x_element.text
+    y = calc(x)
+
+    # Вставляем полученное значение из формулы
+    input1 = browser.find_element(By.CSS_SELECTOR, '#answer')
+    input1.send_keys(y)
+
+    browser.find_element(By.CSS_SELECTOR, 'button').click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
